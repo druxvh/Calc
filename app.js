220572ece60674331b8e0a5a14aef4994173727c
dm <heydruavh@gmail.com>
2023-12-23 19:14:01 +0530

    Bugs fixed & Now Working

--- app.js
+++ app.js
@@ -6,39 +6,40 @@ let operators = document.querySelectorAll(".operator");
 Array.from(buttons).forEach((button) => {
   button.addEventListener("click", (e) => {
     if (e.target.textContent == "=") {
-      val = eval(val);
-      display.value = val;
-      switch (true) {
-        case isNaN(val):
-          alert("The Value is Not a Number");
-          val = "";
-          display.value = val;
-          break;
+        try{
+            val = eval(val);
+            display.value = val;
+            switch (true) {
+              case isNaN(val):
+                val = "";
+                display.value = "Not a Number";
+                break;
+      
+              case val === undefined:
+                val = "";
+                display.value = "Undefined";
+                break;
+      
+              case val === Infinity:
+                val = "";
+                display.value = "Infinity";
+                break;
+            }
+        } 
+        catch(error){
+            val = ""
+            display.value = "Error"
+            
+        }
 
-        case val === undefined:
-          alert("The Value is Undefined");
-          val = "";
-          display.value = val;
-          break;
-
-        case val === Infinity:
-          alert("The Value is Infinity");
-          val = "";
-          display.value = val;
-          break;
-      }
+    
     } else if (e.target.textContent == "AC") {
       val = "";
-      display.value = 0;
+      display.value = "0";
     } else if (e.target.textContent == "Del") {
       val = val.slice(0, -1);
-      // val = val.substring(0, val.length -1)
       display.value = val;
-      if (val == "") {
-        display.value = 0;
-      } else {
-        return 0;
-      }
+      
     } else {
       val += e.target.textContent;
       display.value = val;
